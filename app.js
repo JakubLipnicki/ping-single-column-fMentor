0bc8c831587d64f33049544677e8083ec1204ba1
jakubCode <jakublipnicki1@gmail.com>
2024-01-30 15:18:09 +0100

    error display desktop&mobile

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const input = document.querySelector('.email-input');
 const button = document.querySelector('.submit-button');
-
+const form = document.querySelector('form');
+const errorMessage = document.createElement('p');
 const validateEmail = (email) => {
     return input.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/);
 };
+const errorInsertion = (message) => {
+    errorMessage.textContent = message;
+    errorMessage.classList.add('error-message');
+    console.log(errorMessage);
+    input.after(errorMessage);
+};
 button.addEventListener('click', (e) => {
     if (input.value === '') {
-        console.log('Whoops! It looks like you forgot to add your email');
         e.preventDefault();
+        input.classList.add('input-error');
+        errorInsertion('Whoops! It looks like you forgot to add your email');
         return;
     }
     if (validateEmail(input)) {
-        console.log('match');
+        input.classList.remove('input-error');
+        errorInsertion('');
+        input.value = '';
         e.preventDefault();
         return;
     } else {
-        console.log('error');
         e.preventDefault();
+        input.classList.add('input-error');
+        errorInsertion('Please provide a valid email address');
     }
 });
